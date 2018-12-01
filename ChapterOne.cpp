dcd9057a7ee3327c48bf2e681ddffbbccfa82c6a
Vern Francisco <avernethy@gmail.com>
2018-11-30 22:47:37 -0600

    Completed thru exercise 1.11

--- ChapterOne.cpp
+++ ChapterOne.cpp
@@ -6,7 +6,7 @@ int main()
     std::cout << "Enter two numbers:" << std::endl;
 
     int v1 = 0, v2 = 0;
-    //std::cin >> v1 >> v2;
+    std::cin >> v1 >> v2;
 
     std::cout << "The sum of " << v1 << " and " << v2 << " is: " << v1 + v2 << std::endl;
 
@@ -39,6 +39,11 @@ int main()
     while(count > -1){
         std::cout << count-- << std::endl;
         //count--;
+    }
+
+    //exercise 1.11
+    while (v2 >= v1){
+        std::cout << v2-- <<std::endl;
     }
 
     return 0;
